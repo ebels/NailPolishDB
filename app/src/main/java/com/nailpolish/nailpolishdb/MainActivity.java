308b2bb1658505660e9cdb781da141b8fe96e571
Sarah <sarah.ebelsheiser@gmail.com>
2017-03-08 11:43:40 +0100

    Update
    - Delete "Test-Acitivity" DisplayMessageActivity.java + XML-file
    - Add new (empty) activity "Add new Nailpolish" - addnew,java + XML-file
    - Add new (empty) activity "Viw Nailpolish Database" - viewdatabase.java
    + XML-file
    - MainActivity: add toolbar, add function for buttons, add image
    - Add strings
    - Change AppTheme

--- app/src/main/java/com/nailpolish/nailpolishdb/MainActivity.java
+++ app/src/main/java/com/nailpolish/nailpolishdb/MainActivity.java
@@ -3,6 +3,7 @@ package com.nailpolish.nailpolishdb;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.EditText;
 
@@ -13,15 +14,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        /** sets the toolbar as the app bar for the activity */
+        Toolbar Toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(Toolbar);
     }
 
-    /** Called when the user taps the Send button */
-    public void sendMessage(View view) {
-        // Do something in response to button
-        Intent intent = new Intent(this, DisplayMessageActivity.class);
-        EditText editText = (EditText) findViewById(R.id.editText);
-        String message = editText.getText().toString();
-        intent.putExtra(EXTRA_MESSAGE, message);
+    /** Called when the user taps the "addnew" button */
+    public void addnew (View view) {
+        /** runtime binding between mainactivity and addnew activity */
+        Intent intent = new Intent(this, AddNew.class);
+        startActivity(intent);
+    }
+
+    /** Called when the user taps the "viewdatabase" button */
+    public void viewdatabase (View view) {
+        /** runtime binding between mainactivity and viewdatabase activity */
+        Intent intent = new Intent(this, ViewDatabase.class);
         startActivity(intent);
     }
 }
