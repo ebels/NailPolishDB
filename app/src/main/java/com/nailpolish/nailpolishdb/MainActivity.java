3be94f115722de2087bfbd1c5d4a1da8334c817b
Sarah <sarah.ebelsheiser@gmail.com>
2017-03-08 12:17:06 +0100

    Add Settings menu
    Add Settings menu to toolbar

--- app/src/main/java/com/nailpolish/nailpolishdb/MainActivity.java
+++ app/src/main/java/com/nailpolish/nailpolishdb/MainActivity.java
@@ -4,12 +4,15 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
-    public static final String EXTRA_MESSAGE = "com.nailpolish.nailpolishdb.MESSAGE";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -17,6 +20,14 @@ public class MainActivity extends AppCompatActivity {
         /** sets the toolbar as the app bar for the activity */
         Toolbar Toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(Toolbar);
+    }
+
+    //Settings menu in toolbar
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_main, menu);
+        return true;
     }
 
     /** Called when the user taps the "addnew" button */
