d2aad7608660c1b5b676590baa42286e3c754472
Sarah <sarah.ebelsheiser@gmail.com>
2017-03-08 12:34:30 +0100

    Change "add new" activity
    - add toolbar
    - add "up" button to activity
    - show title "Add new Nailpolish"

--- app/src/main/java/com/nailpolish/nailpolishdb/AddNew.java
+++ app/src/main/java/com/nailpolish/nailpolishdb/AddNew.java
@@ -1,7 +1,9 @@
 package com.nailpolish.nailpolishdb;
 
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 public class AddNew extends AppCompatActivity {
 
@@ -9,5 +11,15 @@ public class AddNew extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_new);
+
+        /** sets the toolbar as the app bar for the activity */
+        Toolbar Toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(Toolbar);
+
+        // Get a support ActionBar corresponding to this toolbar
+        ActionBar arrowup = getSupportActionBar();
+
+        // Enable the Up button
+        arrowup.setDisplayHomeAsUpEnabled(true);
     }
 }
