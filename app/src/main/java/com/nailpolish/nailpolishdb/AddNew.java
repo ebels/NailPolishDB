35f71c7b21d1b1991001cfd4f9e44c09caaa0f88
Sarah <sarah.ebelsheiser@gmail.com>
2017-03-14 16:10:24 +0100

    Add Spinner
    - Add spinner to activity (color + finish)

--- app/src/main/java/com/nailpolish/nailpolishdb/AddNew.java
+++ app/src/main/java/com/nailpolish/nailpolishdb/AddNew.java
@@ -1,26 +1,45 @@
 package com.nailpolish.nailpolishdb;
 
-import android.app.Activity;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class AddNew extends AppCompatActivity {
+
+    private Spinner spinnercolor, spinnerfinish;
+    private ArrayAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_new);
 
+        /* ------- TOOLBAR ------- */
         /** sets the toolbar as the app bar for the activity */
         Toolbar Toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(Toolbar);
-
         // Get a support actionBar corresponding to this toolbar
         ActionBar arrowup = getSupportActionBar();
-
         // Enable the "up" button
         arrowup.setDisplayHomeAsUpEnabled(true);
+
+        /* ------- SPINNER NAILPOLISH COLOR ------- */
+        // get the selected dropdown list value - Spinner element
+        spinnercolor = (Spinner) findViewById(R.id.spinner_color);
+        // Create an ArrayAdapter using the string array and a default spinner layout
+        adapter = ArrayAdapter.createFromResource(this,R.array.npcolor_arrays, android.R.layout.simple_spinner_item);
+        // Specify the layout to use when the list of choices appears
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        // Apply the adapter to the spinner
+        spinnercolor.setAdapter(adapter);
+
+        /* ------- SPINNER NAILPOLISH FINISH ------- */
+        spinnerfinish = (Spinner) findViewById(R.id.spinner_finish);
+        adapter = ArrayAdapter.createFromResource(this, R.array.npfinish_arrays, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinnerfinish.setAdapter(adapter);
     }
 }
