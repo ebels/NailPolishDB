1a4b12ecbf761a2ed69af79ad08885298070c83c
Sarah <sarah.ebelsheiser@gmail.com>
2017-03-15 15:41:01 +0100

    Add Toolbar to PreferenceActivity
    Add Toolbar to PreferenceActivity

--- app/src/main/java/com/nailpolish/nailpolishdb/Settings.java
+++ app/src/main/java/com/nailpolish/nailpolishdb/Settings.java
@@ -5,23 +5,77 @@ package com.nailpolish.nailpolishdb;
  */
 
 import android.os.Bundle;
-import android.preference.Preference;
 import android.preference.PreferenceActivity;
-import android.widget.Toast;
+import android.support.annotation.LayoutRes;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatDelegate;
+import android.support.v7.widget.Toolbar;
 
-public class Settings extends PreferenceActivity implements Preference.OnPreferenceChangeListener {
+/*public class Settings extends PreferenceActivity implements Preference.OnPreferenceChangeListener {
 
-    @Override
+   /* @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         addPreferencesFromResource(R.xml.preferences);
-        Toast.makeText(this, "Einstellungen-Activity gestartet.", Toast.LENGTH_SHORT).show();
-        Toast.makeText(this, "Zurück mit Back-Button.", Toast.LENGTH_SHORT).show();
     }
 
     @Override
     public boolean onPreferenceChange(Preference preference, Object value) {
         return false;
+    }
+}*/
+
+public class Settings extends PreferenceActivity {
+
+    private AppCompatDelegate mDelegate;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        getDelegate().installViewFactory();
+        getDelegate().onCreate(savedInstanceState);
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.setting_toolbar);
+        setSupportActionBar((Toolbar) findViewById(R.id.toolbar));
+        addPreferencesFromResource(R.xml.preferences);
+    }
+
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        getDelegate().onPostCreate(savedInstanceState);
+    }
+
+    @Override
+    public void setContentView(@LayoutRes int layoutResID) {
+        getDelegate().setContentView(layoutResID);
+    }
+
+    @Override
+    protected void onPostResume() {
+        super.onPostResume();
+        getDelegate().onPostResume();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        getDelegate().onStop();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        getDelegate().onDestroy();
+    }
+
+    private void setSupportActionBar(@Nullable Toolbar toolbar) {
+        getDelegate().setSupportActionBar(toolbar);
+    }
+
+    private AppCompatDelegate getDelegate() {
+        if (mDelegate == null) {
+            mDelegate = AppCompatDelegate.create(this, null);
+        }
+        return mDelegate;
     }
 }
