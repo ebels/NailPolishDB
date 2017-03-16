5e4ad36925748c4a9df550af296d7a7fe8941c62
Sarah <sarah.ebelsheiser@gmail.com>
2017-03-16 16:50:24 +0100

    Add toolbar to viewdatabase activity

--- app/src/main/java/com/nailpolish/nailpolishdb/ViewDatabase.java
+++ app/src/main/java/com/nailpolish/nailpolishdb/ViewDatabase.java
@@ -1,7 +1,9 @@
 package com.nailpolish.nailpolishdb;
 
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 public class ViewDatabase extends AppCompatActivity {
 
@@ -9,5 +11,14 @@ public class ViewDatabase extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_database);
+
+        /* ------- TOOLBAR ------- */
+        /** sets the toolbar as the app bar for the activity */
+        Toolbar Toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(Toolbar);
+        // Get a support actionBar corresponding to this toolbar
+        ActionBar arrowup = getSupportActionBar();
+        // Enable the "up" button
+        arrowup.setDisplayHomeAsUpEnabled(true);
     }
 }
