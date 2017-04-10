58fd27ddfcba34bb30a57c460725dc53a4e024ec
Sarah <sarah.ebelsheiser@gmail.com>
2017-04-10 14:12:46 +0200

    Create Detail View of nail Polish

---
+++ app/src/main/java/com/nailpolish/nailpolishdb/Details.java
@@ -0,0 +1,35 @@
+package com.nailpolish.nailpolishdb;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.widget.TextView;
+
+/**
+ * Created by ebelsheiser on 10.04.2017.
+ */
+
+public class Details extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_details);
+        /** sets the toolbar as the app bar for the activity */
+        Toolbar Toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(Toolbar);
+        ActionBar arrowup = getSupportActionBar(); // Get a support actionBar corresponding to this toolbar
+        arrowup.setDisplayHomeAsUpEnabled(true); // Enable the "up" button
+
+        TextView tvname = (TextView) findViewById(R.id.textView_name);
+        Intent intent = getIntent();
+        String name = intent.getStringExtra("item");
+        tvname.setText(name);
+
+        //Set custom Toolbar title
+        Toolbar.setTitle("Details " + name);
+
+    }
+}
