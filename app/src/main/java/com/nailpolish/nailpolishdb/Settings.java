831d2ed345bdb41531d04c0f1e88c0522509a4e9
Sarah <sarah.ebelsheiser@gmail.com>
2017-04-13 12:37:24 +0200

    Remove unused code

--- app/src/main/java/com/nailpolish/nailpolishdb/Settings.java
+++ app/src/main/java/com/nailpolish/nailpolishdb/Settings.java
@@ -10,6 +10,7 @@ import android.preference.Preference;
 import android.preference.PreferenceActivity;
 import android.support.annotation.LayoutRes;
 import android.support.annotation.Nullable;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatDelegate;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
@@ -26,58 +27,12 @@ public class Settings extends PreferenceActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         addPreferencesFromResource(R.xml.preferences);
+        setContentView(R.layout.setting_toolbar);
+
 
         //todo: get nail polish count + set in preferences
         ViewDatabase view = new ViewDatabase();
         Preference preference = (Preference) findPreference("nailpolish_count");
-        preference.setSummary(view.countNPs());
-
-        /*
-        setContentView(R.layout.setting_toolbar);
-        setSupportActionBar((Toolbar) findViewById(toolbar));
-        getDelegate().installViewFactory();
-        getDelegate().onCreate(savedInstanceState);*/
+        //preference.setSummary(view.countNPs());
     }
-
-    /*
-    @Override
-    protected void onPostCreate(Bundle savedInstanceState) {
-        super.onPostCreate(savedInstanceState);
-        getDelegate().onPostCreate(savedInstanceState);
-    }
-
-    @Override
-    public void setContentView(@LayoutRes int layoutResID) {
-        getDelegate().setContentView(layoutResID);
-    }
-
-    @Override
-    protected void onPostResume() {
-        super.onPostResume();
-        getDelegate().onPostResume();
-    }
-
-    @Override
-    protected void onStop() {
-        super.onStop();
-        getDelegate().onStop();
-    }
-
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        getDelegate().onDestroy();
-    }
-
-    private void setSupportActionBar(@Nullable Toolbar toolbar) {
-        getDelegate().setSupportActionBar(toolbar);
-    }
-
-    private AppCompatDelegate getDelegate() {
-        if (mDelegate == null) {
-            mDelegate = AppCompatDelegate.create(this, null);
-        }
-        return mDelegate;
-    }
-    */
 }
